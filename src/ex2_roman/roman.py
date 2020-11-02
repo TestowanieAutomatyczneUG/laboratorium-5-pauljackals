c598b02fef092ba5140e0a3ce7a74a83cf13f89c
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-02 21:27:27 +0100

    refactored to work with numbers smaller than 90

--- src/ex2_roman/roman.py
+++ src/ex2_roman/roman.py
@@ -1,10 +1,18 @@
+import math
+
+
 def roman(number):
-    if number < 40:
+    if number < 90:
         symbols = {
+            50: 'L',
             10: 'X',
             5: 'V',
             1: 'I'
         }
+        symbols_10s = []
+        for symbol in symbols.keys():
+            if math.log10(symbol).is_integer():
+                symbols_10s.append(symbol)
         temp = number
         roman_number = ''
         while temp > 0:
@@ -13,14 +21,14 @@ def roman(number):
                     roman_number += value
                     temp -= key
                     break
-                elif temp + 1 - key == 0:
-                    roman_number += (symbols.get(1) + value)
-                    temp -= key - 1
-                    break
+                else:
+                    flag = False
+                    for key_10s in symbols_10s:
+                        if key_10s < temp and temp + key_10s - key >= 0:
+                            roman_number += (symbols.get(key_10s) + value)
+                            temp -= (key - key_10s)
+                            flag = True
+                            break
+                    if flag:
+                        break
         return roman_number
-    elif number == 48:
-        return 'XL'+roman(8)
-    elif number == 49:
-        return 'XL'+roman(9)
-    elif number == 59:
-        return 'L'+roman(9)
