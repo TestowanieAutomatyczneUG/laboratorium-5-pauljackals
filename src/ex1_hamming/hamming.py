c1994a8425ebec81817e54e02e3b81296cd933b9
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-02 16:53:50 +0100

    refactored distance method to work with any strands

--- src/ex1_hamming/hamming.py
+++ src/ex1_hamming/hamming.py
@@ -1,13 +1,8 @@
 class Hamming:
     def distance(self, string1, string2):
-        if string1 == "" and string2 == "":
-            return 0
-        elif string1 == 'A' and string2 == 'A':
-            return 0
-        elif string1 == 'G' and string2 == 'T':
-            return 1
-        elif string1 == 'GGACTGAAATCTG' and string2 == 'GGACTGAAATCTG':
-            return 0
-        elif string1 == 'GGACGGATTCTG' and string2 =='AGGACGGATTCT':
-            return 9
+        difference = 0
+        for i in range(len(string1)):
+            if string1[i] != string2[i]:
+                difference += 1
+        return difference
 
