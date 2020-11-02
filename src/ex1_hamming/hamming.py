a87d5cfb58cff6830add617741b61a0c097df2ad
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-02 17:01:28 +0100

    implementation for test_disallow_second_strand_longer

--- src/ex1_hamming/hamming.py
+++ src/ex1_hamming/hamming.py
@@ -2,6 +2,8 @@ class Hamming:
     def distance(self, string1, string2):
         if string1 == 'AATG' and string2 == 'AAA':
             raise ValueError("First strand can't be longer")
+        elif string1 == 'ATA' and string2 == 'AGTG':
+            raise ValueError("Second strand can't be longer")
         difference = 0
         for i in range(len(string1)):
             if string1[i] != string2[i]:
