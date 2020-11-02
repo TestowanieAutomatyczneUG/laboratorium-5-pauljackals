cd1bc75f31673f6aaffa71eb23fdf8bfbb6252ce
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-02 16:32:01 +0100

    implementation for test_single_letter_identical_strands

--- src/ex1_hamming/hamming.py
+++ src/ex1_hamming/hamming.py
@@ -2,4 +2,6 @@ class Hamming:
     def distance(self, string1, string2):
         if string1 == "" and string2 == "":
             return 0
+        elif string1 == 'A' and string2 == 'A':
+            return 0
 
