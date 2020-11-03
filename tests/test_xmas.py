eb021e65d32cf0bee68e7f42f29a894765140b78
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-03 18:48:09 +0100

    testing verse 11

--- tests/test_xmas.py
+++ tests/test_xmas.py
@@ -33,6 +33,9 @@ class XmasTest(unittest.TestCase):
     def test_verse_10(self):
         self.assertEqual(Xmas.get_verse(10), 'On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
 
+    def test_verse_11(self):
+        self.assertEqual(Xmas.get_verse(11), 'On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
+
 
 if __name__ == '__main__':
     unittest.main()
