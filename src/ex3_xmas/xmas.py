4c00fe083420f80a61a424ed5c772d8d84937da1
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-03 19:28:42 +0100

    merged gifts and numerals | made upper bound dependent on gifts length

--- src/ex3_xmas/xmas.py
+++ src/ex3_xmas/xmas.py
@@ -1,40 +1,27 @@
 class Xmas:
 
-    @staticmethod
-    def __text(index):
-        numerals = [
-            'first',
-            'second',
-            'third',
-            'fourth',
-            'fifth',
-            'sixth',
-            'seventh',
-            'eighth',
-            'ninth',
-            'tenth',
-            'eleventh',
-            'twelfth'
-        ]
-        gifts = [
-            'a Partridge in a Pear Tree',
-            'two Turtle Doves',
-            'three French Hens',
-            'four Calling Birds',
-            'five Gold Rings',
-            'six Geese-a-Laying',
-            'seven Swans-a-Swimming',
-            'eight Maids-a-Milking',
-            'nine Ladies Dancing',
-            'ten Lords-a-Leaping',
-            'eleven Pipers Piping',
-            'twelve Drummers Drumming'
-        ]
-        verse = 'On the ' + numerals[index] + ' day of Christmas my true love gave to me: '
+    __gifts = [
+        ('first', 'a Partridge in a Pear Tree'),
+        ('second', 'two Turtle Doves'),
+        ('third', 'three French Hens'),
+        ('fourth', 'four Calling Birds'),
+        ('fifth', 'five Gold Rings'),
+        ('sixth', 'six Geese-a-Laying'),
+        ('seventh', 'seven Swans-a-Swimming'),
+        ('eighth', 'eight Maids-a-Milking'),
+        ('ninth', 'nine Ladies Dancing'),
+        ('tenth', 'ten Lords-a-Leaping'),
+        ('eleventh', 'eleven Pipers Piping'),
+        ('twelfth', 'twelve Drummers Drumming'),
+    ]
+
+    @classmethod
+    def __text(cls, index):
+        verse = 'On the ' + cls.__gifts[index][0] + ' day of Christmas my true love gave to me: '
         for i in range(index, -1, -1):
             if i == 0 and index > 0:
                 verse += 'and '
-            verse += gifts[i]
+            verse += cls.__gifts[i][1]
             if i == 0:
                 verse += '.'
             else:
@@ -43,12 +30,12 @@ class Xmas:
 
     @classmethod
     def get_verse(cls, number):
-        if 1 <= number <= 12:
+        if 1 <= number <= len(cls.__gifts):
             return cls.__text(number-1)
 
     @classmethod
     def get_verses(cls, start, end):
-        if start <= end and 1 <= start <= 12 and 1 <= end <= 12:
+        if 1 <= start <= end <= len(cls.__gifts):
             string = ''
             for i in range(start, end+1):
                 string += cls.get_verse(i)
