be5e28c3d2999a46d4bba335259ed7eb3cf99828
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-03 18:03:50 +0100

    implementation for test_verse_1

---
+++ src/ex3_xmas/xmas.py
@@ -0,0 +1,6 @@
+class Xmas:
+
+    @staticmethod
+    def get_verse(number):
+        if number == 1:
+            return 'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.'
