916022d84dbded580888302f061eb7cf91a1aa75
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-03 18:08:16 +0100

    implementation for test_verse_2

--- src/ex3_xmas/xmas.py
+++ src/ex3_xmas/xmas.py
@@ -4,3 +4,5 @@ class Xmas:
     def get_verse(number):
         if number == 1:
             return 'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.'
+        elif number == 2:
+            return 'On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.'
--- tests/test_xmas.py
+++ tests/test_xmas.py
@@ -7,7 +7,7 @@ class XmasTest(unittest.TestCase):
         self.assertEqual(Xmas.get_verse(1), 'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.')
 
     def test_verse_2(self):
-        self.assertEqual(Xmas.get_verse(2), 'On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree')
+        self.assertEqual(Xmas.get_verse(2), 'On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.')
 
 
 if __name__ == '__main__':
