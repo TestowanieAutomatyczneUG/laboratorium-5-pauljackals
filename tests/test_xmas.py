27558a55e01d88264e47d4e651f2f2aaa3727626
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-03 19:10:15 +0100

    testing verses 8 to 8

--- tests/test_xmas.py
+++ tests/test_xmas.py
@@ -44,6 +44,9 @@ class XmasTest(unittest.TestCase):
                                                 'On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n\n'
                                                 'On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
 
+    def test_verses_8_to_8(self):
+        self.assertEqual(Xmas.get_verses(8, 8), 'On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
+
 
 if __name__ == '__main__':
     unittest.main()
