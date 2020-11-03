8047f01d86909cf7d610955ac35eca60137a9235
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-03 18:09:16 +0100

    testing verse 3

--- tests/test_xmas.py
+++ tests/test_xmas.py
@@ -9,6 +9,8 @@ class XmasTest(unittest.TestCase):
     def test_verse_2(self):
         self.assertEqual(Xmas.get_verse(2), 'On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.')
 
+    def test_verse_3(self):
+        self.assertEqual(Xmas.get_verse(3), 'On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
 
 if __name__ == '__main__':
     unittest.main()
