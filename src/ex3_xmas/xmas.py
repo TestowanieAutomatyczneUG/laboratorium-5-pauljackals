04e68250e85f004158253f3462939817ebf4810f
Pawel Sakaluk <paul.jackals@gmail.com>
2020-11-03 18:13:23 +0100

    implementation for test_verse_5

--- src/ex3_xmas/xmas.py
+++ src/ex3_xmas/xmas.py
@@ -10,3 +10,5 @@ class Xmas:
             return 'On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'
         elif number == 4:
             return 'On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'
+        elif number == 5:
+            return 'On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'
